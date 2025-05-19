c76c1d757abc83a98e2edf525be9f1c18063106a
Cyteon <129582290+Cyteon@users.noreply.github.com>
2025-05-19 10:47:17 +0000

    initial design

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,3 @@
+<nav class="p-2 w-full h-fit border-b border-black/40">
+    <h1 class="text-xl font-bold">Fractal</h1>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<div class="w-full h-full flex-row bg-base rounded-md border border-black/40">
+    <Navbar />
+    <div class="p-2">
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    </div>
+</div>
