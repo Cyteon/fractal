488e08f9662ec20268297f4d77e9149a6f880fee
Cyteon <129582290+Cyteon@users.noreply.github.com>
2025-05-19 11:17:07 +0000

    margin fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-full h-full flex-row bg-base rounded-md border border-black/40">
     <Navbar />
-    <div class="p-2">
+    <div class="p-2 px-3">
         <h1>Welcome to SvelteKit</h1>
         <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
     </div>
